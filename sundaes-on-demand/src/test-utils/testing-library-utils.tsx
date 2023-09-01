3c718b04fdffb2139c1e463501c59d5c56102399
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-09-01 11:22:26 +0200

    Coverted testing-lib utils to typescript

--- sundaes-on-demand/src/test-utils/testing-library-utils.tsx
+++ sundaes-on-demand/src/test-utils/testing-library-utils.tsx
@@ -1,7 +1,9 @@
-import { render } from "@testing-library/react";
+import { RenderOptions, render } from "@testing-library/react";
 import { OrderDetailsProvider } from "../contexts/OrderDetails";
+import { JSX } from "react/jsx-runtime";
 
-const renderWithContext = (ui, options) =>
+
+const renderWithContext = (ui: JSX.Element, options?: Omit<RenderOptions,"Wrapper">) =>
   render(ui, { wrapper: OrderDetailsProvider, ...options });
 
 // re-export everything
